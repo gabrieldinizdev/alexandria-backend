d31347917e664df17b6950080da0a4dd3fe3c495
marcelo wesley <marcelo_oliscott@hotmail.com>
2024-07-14 00:31:29 -0300

    (Config): initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      cache: true,
+      envFilePath: '.env',
+    }),
+    ModulesModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  const serverPort = configService.getOrThrow('SERVER_PORT');
+
+  const TITLE = 'Project - Alexandria';
+  const DESCRIPTION = 'The main API of "Alexandria"';
+  const API_VERSION = '1.0';
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle(TITLE)
+    .setDescription(DESCRIPTION)
+    .setVersion(API_VERSION)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  });
+
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: TITLE,
+  });
+
+  await app.listen(serverPort);
+
+  const url = await app.getUrl();
+  logger.log(`listening app at ${url}`);
 }
+
 bootstrap();
