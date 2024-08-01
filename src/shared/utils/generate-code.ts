1fbe4624444ebfb7dae2336c2c74e1e707f7be08
marcelo wesley <marcelo_oliscott@hotmail.com>
2024-08-01 15:25:33 -0300

    review(shared): add code generator with crypto

--- src/shared/utils/generate-code.ts
+++ src/shared/utils/generate-code.ts
@@ -1,9 +1,10 @@
+import * as crypto from 'crypto';
+
 function generateCode(quantity: number): string {
   const char = 'abcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
-
   for (let i = 0; i < quantity; i++) {
-    const index = Math.floor(Math.random() * char.length);
+    const index = crypto.randomBytes(1)[0] % char.length;
     result += char[index];
   }
 
