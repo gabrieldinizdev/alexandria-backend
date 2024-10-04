7e8a2c964d48518686c4dc255a8a529a3b55f070
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-10-04 13:41:25 -0300

    test(e2e): add orm fixture example

--- test/department.e2e-spec.ts
+++ test/department.e2e-spec.ts
@@ -1,12 +1,16 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaClient } from '@prisma/client';
+import * as supertest from 'supertest';
+
 import { DepartmentModule } from '@/modules/departments';
 import { AuthGuard } from '@/shared/guards';
 import { appConfiguration } from '@/utils/config';
 
 describe('Department (e2e)', () => {
   let app: INestApplication;
+  const orm = new PrismaClient();
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -23,9 +27,77 @@ describe('Department (e2e)', () => {
     await app.init();
   });
 
+  beforeAll(async () => {
+    await orm.$connect();
+
+    await orm.department.createMany({
+      data: [
+        { name: 'Engineering' },
+        { name: 'Marketing' },
+        { name: 'Sales' },
+        { name: 'Human Resources' },
+        { name: 'Finance' },
+      ],
+    });
+  });
+
   describe('/api/departments (GET)', () => {
     describe('success responses', () => {
-      it.todo('should return an array of departments');
+      it('should return an array of departments', async () => {
+        const sut = await supertest(app.getHttpServer())
+          .get('/api/departments')
+          .expect(200);
+
+        expect(sut.body).toEqual(
+          expect.objectContaining({
+            data: [
+              {
+                id: expect.any(String),
+                name: 'Engineering',
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                deletedAt: null,
+              },
+              {
+                id: expect.any(String),
+                name: 'Marketing',
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                deletedAt: null,
+              },
+              {
+                id: expect.any(String),
+                name: 'Sales',
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                deletedAt: null,
+              },
+              {
+                id: expect.any(String),
+                name: 'Human Resources',
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                deletedAt: null,
+              },
+              {
+                id: expect.any(String),
+                name: 'Finance',
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String),
+                deletedAt: null,
+              },
+            ],
+            meta: {
+              page: expect.any(Number),
+              size: expect.any(Number),
+              total: 5,
+              pageCount: 1,
+              hasPrevious: expect.any(Boolean),
+              hasNext: expect.any(Boolean),
+            },
+          }),
+        );
+      });
 
       it.todo('should return an array of departments with pagination');
 
@@ -42,6 +114,16 @@ describe('Department (e2e)', () => {
   });
 
   afterAll(async () => {
+    await orm.department.deleteMany({
+      where: {
+        id: {
+          not: undefined,
+        },
+      },
+    });
+
+    await orm.$disconnect();
+
     await app.close();
   });
 });
